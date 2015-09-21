3489ad0d8276c4146b8e8147ea9554e93442eb96
Adam Luzsi <adamluzsi@gmail.com>
2015-09-21 15:53:30 +0200

    add support for faraday folder that contain common used middlewares instead of -c option multople times

--- lib/faraday/cli/middleware_fetcher.rb
+++ lib/faraday/cli/middleware_fetcher.rb
@@ -13,24 +13,44 @@ module Faraday::CLI::MiddlewareFetcher
 
   def get_file_paths(config_file_paths)
     file_name = '{.faraday.rb,.faraday}'
+    folder_name = '{.faraday.rb,.faraday,.faraday-cli}'
+
     case
 
       when !config_file_paths.empty?
-        config_file_paths
+        config_file_paths.reduce([]) do |file_paths, given_path|
+          file_paths.push(*Dir.glob(given_path)); file_paths
+        end
 
       when !(file_paths = Dir.glob(File.join(Dir.pwd, file_name))).empty?
+        file_paths
+
+      when !(file_paths = folder_content(Dir.pwd, folder_name)).empty?
         file_paths
 
       when !(file_paths = Dir.glob(File.join(PWD.pwd, file_name))).empty?
         file_paths
 
+      when !(file_paths = folder_content(PWD.pwd, folder_name)).empty?
+        file_paths
+
       when !(file_paths = Dir.glob(File.join(ENV['HOME'], file_name))).empty?
+        file_paths
+
+      when !(file_paths = folder_content(ENV['HOME'], folder_name)).empty?
         file_paths
 
       else
         []
 
     end
+  end
+
+
+  protected
+
+  def folder_content(main_path, folder_name)
+    Dir.glob(File.join(main_path, folder_name, '*.rb')).select { |path| not File.directory?(path) }
   end
 
 end
