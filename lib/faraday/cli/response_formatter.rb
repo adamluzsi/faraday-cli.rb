543ca6ddd23b3135e825e709e70cce97397ef3c0
Adam Luzsi <adamluzsi@gmail.com>
2015-09-21 16:25:02 +0200

    verbose option for less talkative logging

--- lib/faraday/cli/response_formatter.rb
+++ lib/faraday/cli/response_formatter.rb
@@ -2,9 +2,9 @@ require 'yaml'
 module Faraday::CLI::ResponseFormatter
   extend self
 
-  def format(faraday_response)
+  def format(faraday_response,*flags)
     formatted_message_parts = []
-    formatted_message_parts << YAML.dump(response_hash_by(faraday_response))
+    formatted_message_parts << YAML.dump(response_hash_by(faraday_response)) if flags.include?(:verbose)
     formatted_message_parts << faraday_response.body
 
     formatted_message_parts.join("\n")
